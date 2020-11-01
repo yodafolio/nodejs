cc4a84f36389cb4ac965c31729fa0739c896ee47
YEON SOO BAE <yeonsoobae@YEONui-MacBook-Pro.local>
2020-11-01 12:27:38 +0900

    파일추가

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
 var http = require('http');
+var fs = require('fs');
+var log = console.log;
 
 var app = http.createServer((req, res) => {
-    console.log('hello');
+    var url = req.url;
+    if(url === '/') {
+        url = '/index.html';
+    }
+    if(url === 'favicon.ico') {
+        res.writeHead(404);
+        res.end();
+        return;
+    }
+    res.writeHead(200);
+    var data = fs.readFileSync(__dirname + url);
+    log(data)
+    res.end(data);
 });
+
+
 
 app.listen(3000);
