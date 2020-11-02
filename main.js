f5867a8038eaa548dbe8e380b0b55efb67058f57
YEON SOO BAE <yeonsoobae@YEONui-MacBook-Pro.local>
2020-11-02 17:24:31 +0900

    node.js

--- main.js
+++ main.js
@@ -1,36 +1,8 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var log = console.log;
-
-var templateHTML = (title, list, body) => {
-    return `
-    <!doctype html>
-    <html>
-    <head>
-    <title>WEB1 - ${title}</title>
-    <meta charset="utf-8">
-    </head>
-    <body>
-    <h1><a href="/">WEB</a></h1>
-    ${list}
-    <a href="/create">create</a>
-    ${body}
-    </body>
-    </html>
-    `
-}
-
-var templateList = (filelist) => {
-    var list = '<ul>';
-    var i = 0;
-    while(i < filelist.length) {
-        list = list + `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
-        i = i + 1;
-    }
-    list = list + '</ul>';
-    return list
-}
+var qs = require('querystring');
+var template = require('./template/template.js');
 
 var app = http.createServer((req, res) => {
     var _url = req.url;
@@ -40,68 +12,131 @@ var app = http.createServer((req, res) => {
 
     if(pathname === '/') {
         if(queryData.id === undefined) {
-
             fs.readdir('./data', (err, filelist) => {
-                console.log(filelist);
-                var list = templateList(filelist);
+                var list = template.list(filelist);
                 var title = 'hello';
                 var description = 'hollo, nodejs';
-                var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+                var html = template.HTML(title, list, `<h2>${title}</h2><p>${description}</p>`, '<a href="/create">create</a>');
                 res.writeHead(200);
-                res.end(template);
+                res.end(html);
             })
         } else {
             fs.readdir('./data', (err, filelist) => {
-                console.log(filelist);
-                var list = templateList(filelist);
                 fs.readFile(`data/${queryData.id}`, 'utf-8', (err, data) => {
+                    var list = template.list(filelist);
                     var description = data;
-                    var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+                    var html = template.HTML(title, list,
+                        `<h2>${title}</h2><p>${description}</p>`,
+                        `<a href="/create">create</a>
+                        <a href="/update?id=${title}">update</a>
+                        <form action="/delete_proccess" method="post">
+                            <input type="hidden" name="id" value="${title}">
+                            <input type="submit" value="delete">
+                        </form>
+                        `);
                     res.writeHead(200);
-                    res.end(template);
+                    res.end(html);
                 });
-            })
+            });
         }
     } else if(pathname === '/create') {
-            var template = `
-            <!doctype html>
-            <html>
-            <head>
-            <title>WEB1 - ${title}</title>
-            <meta charset="utf-8">
-            </head>
-            <body>
-            <h1><a href="/">WEB</a></h1>
-            <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-            </ul>
-            <h2>${title}</h2>
-            <p>
-            <form action="" method="">
-            <p>
-                <input type="text" name="title" placeholeder="title">
-            </p>
-            <p>
-                <textarea></textarea>
-            </p>
-            <p>
-                <input type="submit"value="전송">
-            </p>
+        fs.readdir('./data', (err, filelist) => {
+            var title = "WEB - create";
+            var list = template.list(filelist);
+            var html = template.HTML(title, list,
+            `
+            <form action="http://localhost:3000/create_process" method="post">
+                <p>
+                    <input type="text" name="title" placeholeder="title">
+                </p>
+                <p>
+                    <textarea name="description"></textarea>
+                </p>
+                <p>
+                    <input type="submit"value="전송">
+                </p>
             </form>
-            </p>
-            </body>
-            </html>
-            `;
+            `);
             res.writeHead(200);
-            res.end(template);
+            res.end(html);
+        });
+    } else if(pathname === '/create_process') {
+        var body = '';
+        req.on('data', data => {
+            body = body + data;
+        });
+        req.on('end', () => {
+            var post = qs.parse(body);
+            var title = post.title;
+            var description = post.description;
+
+            fs.writeFile(`data/${title}`, description, 'utf-8', err => {
+                res.writeHead(302, {
+                    'Location': `/?id=${title}`
+                });
+                res.end('success');
+            });
+        })
     } else if(pathname === '/update') {
-        res.writeHead(200);
-        res.end('update');
-    } else if(pathname === '/delete') {
-        res.writeHead(200);
-        res.end('delete');
+        fs.readdir('./data', (err, filelist) => {
+            fs.readFile(`data/${queryData.id}`, 'utf-8', (err, data) => {
+                var title = queryData.id;
+                var list = template.list(filelist);
+                var description = data;
+                var html = template.HTML(title, list,
+                    `
+                    <form action="/update_process" method="post">
+                        <input type="hidden" name="id" value="${title}">
+                        <p>
+                            <input type="text" name="title" placeholeder="title" value="${title}">
+                        </p>
+                        <p>
+                            <textarea name="description">${description}</textarea>
+                        </p>
+                        <p>
+                            <input type="submit"value="전송">
+                        </p>
+                    </form>
+                    `,
+                    `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
+                res.writeHead(200);
+                res.end(html);
+            });
+        });
+    } else if(pathname === '/update_process') {
+        var body = '';
+        req.on('data', data => {
+            body = body + data;
+        });
+        req.on('end', () => {
+            var post = qs.parse(body);
+            var id = post.id;
+            var title = post.title;
+            var description = post.description;
+            fs.rename(`data/${id}`, `data/${title}`, err => {
+                fs.writeFile(`data/${title}`, description, 'utf-8', err => {
+                    res.writeHead(302, {
+                        'Location': `/?id=${title}`
+                    });
+                    res.end();
+                });
+            });
+        });
+    } else if(pathname === '/delete_proccess') {
+        var body = '';
+        req.on('data', data => {
+            body = body + data;
+        });
+        req.on('end', () => {
+            var post = qs.parse(body);
+            var id = post.id;
+            fs.unlink(`data/${id}`, err => {
+                res.writeHead(302, {
+                    'Location' : `/`
+                });
+                res.end();
+            })
+        });
     } else {
         res.writeHead(404);
         res.end('not found');
